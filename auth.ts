f871b564621f7382fd8663ecb4cb4b171d9ac6e4
Matthew Marshall <mamarshall44@gmail.com>
2025-09-29 15:53:19 -0700

    trusthost set to true

--- auth.ts
+++ auth.ts
@@ -1,4 +1,4 @@
-import NextAuth from 'next-auth';
+import NextAuth, { NextAuthConfig } from 'next-auth';
 import Github from 'next-auth/providers/github';
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import { prisma } from './lib/prisma';
@@ -6,4 +6,5 @@ import { prisma } from './lib/prisma';
 export const { auth, handlers, signIn, signOut } = NextAuth({
   providers: [Github],
   adapter: PrismaAdapter(prisma),
+  trustHost: true,
 });
